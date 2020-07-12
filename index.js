cdc2f20ed3a76fb96d4d1673900a54d9d71e6ab3
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-07-12 21:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('app').querySelectorAll('.nested')
+  return document.getElementById('app').querySelectorAll('.nested div ')
 }
