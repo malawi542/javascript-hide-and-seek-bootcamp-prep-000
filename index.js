d504c1103be6719a425af17fb6aa5e1d380574a1
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-07-12 22:01:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementsById('app').querySelector('.nested div #target')
+  return document.getElementsById('app').querySelector('main.nested div #target')
 }
